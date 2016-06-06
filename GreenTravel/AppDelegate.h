0c4f9a9dc12df4f2b15f3f9d6eb5a66091bad067
dorayo <yomingyo@gmail.com>
2016-06-07 00:32:45 +0800

    create object

---
+++ GreenTravel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GreenTravel
+//
+//  Created by 彭凯锋 on 16/6/7.
+//  Copyright © 2016年 pengkaifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GreenTravel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GreenTravel
+//
+//  Created by 彭凯锋 on 16/6/7.
+//  Copyright © 2016年 pengkaifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
